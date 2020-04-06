d3e97742aed8a2cc8dadae865678b9897e95f5df
nandrasaputra <nandra.soack@gmail.com>
2020-04-06 23:32:22 +0700

    V 0.0.0.2
    + Major UI Improvements

---
+++ app/src/main/java/com/nandra/covid19id/viewmodel/SharedViewModel.kt
@@ -0,0 +1,7 @@
+package com.nandra.covid19id.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class SharedViewModel(app: Application) : AndroidViewModel(app) {
+}
