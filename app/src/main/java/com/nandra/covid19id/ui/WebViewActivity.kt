bdb87805b8e33f075d543b159461c494b3a4ec88
nandrasaputra <nandra.soack@gmail.com>
2020-04-11 22:41:47 +0700

    V 0.0.3.0
    + Major Improvements In WebViewActivity
    + WebActivity Now Has Menu, Can Translate, Can Share, Can Open In External Browser

--- app/src/main/java/com/nandra/covid19id/ui/WebViewActivity.kt
+++ app/src/main/java/com/nandra/covid19id/ui/WebViewActivity.kt
@@ -1,22 +1,30 @@
 package com.nandra.covid19id.ui
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.res.Configuration
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.nandra.covid19id.R
 import com.nandra.covid19id.utils.Constant
 import kotlinx.android.synthetic.main.activity_web_view.*
+import java.net.URLEncoder
 
 class WebViewActivity : AppCompatActivity() {
 
-    var extraWebsite = "https://www.google.com/"
-    var extraWebTitle = "COVID-19 ID"
+    private var extraWebsite = "https://www.google.com/"
+    private var extraWebTitle = "COVID-19 ID"
+    private var currentUrl = "www.google.com"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,10 +46,14 @@ class WebViewActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
+        onBackNavigationPressed()
+    }
+
+    private fun onBackNavigationPressed() {
         if (activity_web_view_container.canGoBack()) {
             activity_web_view_container.goBack()
         } else {
-            super.onBackPressed()
+            finish()
         }
     }
 
@@ -53,10 +65,11 @@ class WebViewActivity : AppCompatActivity() {
         }
         activity_web_view_toolbar.setNavigationIcon(R.drawable.ic_arrow_back_toolbar)
         activity_web_view_toolbar.setNavigationOnClickListener {
-            finish()
+            onBackNavigationPressed()
         }
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     private fun setupWebView() {
         activity_web_view_container.settings.apply {
             javaScriptEnabled = true
@@ -72,11 +85,23 @@ class WebViewActivity : AppCompatActivity() {
 
     private inner class CustomWebViewClient : WebViewClient() {
         override fun onPageFinished(view: WebView?, url: String?) {
+
+            currentUrl = url ?: "www.google.com"
+
             activity_web_view_progress_bar.visibility = View.GONE
+
+            activity_web_view_toolbar_sub_title.apply {
+                visibility = View.VISIBLE
+                text = url
+            }
+
             super.onPageFinished(view, url)
         }
 
         override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+
+            currentUrl = url ?: "www.google.com"
+
             activity_web_view_toolbar_sub_title.apply {
                 visibility = View.VISIBLE
                 text = url
@@ -95,5 +120,52 @@ class WebViewActivity : AppCompatActivity() {
                 activity_web_view_progress_bar.progress = newProgress
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_web_view_toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.activity_web_view_toolbar_share_menu_item -> {
+                val contentText = "$extraWebTitle :\n$currentUrl\n\nDownload Aplikasi COVID-19 ID"
+                val sharingIntent = Intent(Intent.ACTION_SEND).apply {
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_SUBJECT, "COVID-19 ID")
+                    putExtra(Intent.EXTRA_TEXT, contentText)
+                }
+                startActivity(Intent.createChooser(sharingIntent, "Share Website Ini"))
+                true
+            }
+            R.id.activity_web_view_toolbar_translate_menu_item -> {
+                translatePage(currentUrl)
+                true
+            }
+            R.id.activity_web_view_toolbar_browser_menu_item -> {
+                val openBrowserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(currentUrl))
+                startActivity(openBrowserIntent)
+                true
+            }
+            R.id.activity_web_view_toolbar_close_menu_item -> {
+                finish()
+                true
+            }
+            else -> {false}
+        }
+    }
+
+    private fun translatePage(url: String) {
+        val baseIdToEnTranslateUrl = "https://translate.google.com/translate?hl=en&tab=wT&sl=en&tl=id&u="
+        if (currentUrl.contains("translate.googleusercontent.com", true)) {
+            Toast.makeText(this, "Website Telah Di Translate", Toast.LENGTH_SHORT).show()
+            return
+        } else if (currentUrl == "https://www.google.com/") {
+            return
+        }
+        val encodedUrl = URLEncoder.encode(url, "utf-8")
+        val completeUrl = baseIdToEnTranslateUrl + encodedUrl
+        activity_web_view_container.loadUrl(completeUrl)
     }
 }
